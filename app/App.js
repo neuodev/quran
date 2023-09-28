01f98cecca7bad106cdd7235ed592f1894f7a833
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-09-28 18:25:23 +0300

    feat(app): add surah names & reciters info

--- app/App.js
+++ app/App.js
@@ -1,17 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import AppHeader from "./components/Layout/Header";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <AppHeader />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
